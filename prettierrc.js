384e674cc562c9e3f7b472a7f137f7e0f6f6e72b
Guilherme Amado <guiii@live.com>
2022-12-09 20:28:24 -0300

    config eslint, prettier

---
+++ prettierrc.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-
 function App() {
   return (
     <div>
